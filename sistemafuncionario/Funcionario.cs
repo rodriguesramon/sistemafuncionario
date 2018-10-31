b33ed0cc02c360e441e0c03318c393ce10f46843
Aluno <aluno@Alunos-MacBook-Pro.local>
2018-10-31 18:22:03 -0400

    Primeiro Commit

---
+++ sistemafuncionario/Funcionario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace sistemafuncionario
+{
+    public class Funcionario
+    {
+        public String Nome { get; set; }
+        public double Salario { get; set; }
+
+
+        string nome;
+
+        public void setNome(string nome){
+            this.nome = nome;
+        }
+
+
+        public double novoSalario(){
+            if(Salario <= 1000){
+                Salario += Salario * 0.3;
+            }else if (Salario <= 1500){
+                Salario += Salario * 0.2;
+            }else if (Salario > 1500){
+                Salario += Salario * 0.1;
+            }
+            return Salario;
+        }
+
+        public Funcionario()
+        {
+        }
+    }
+}
---
+++ sistemafuncionario/Program.cs
@@ -0,0 +1,23 @@
+﻿using System ;
+
+namespace sistemafuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+
+            Console.WriteLine("Nome Funcionario: ");
+            funcionario.Nome = Console.ReadLine();
+
+            Console.WriteLine("Salario Funcionario: ");
+            funcionario.Salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Funcionario: {0} - Salario {1}", 
+                              funcionario.Nome, funcionario.novoSalario());
+
+
+        }
+    }
+}
